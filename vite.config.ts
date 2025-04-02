a76ee16e76eed322f24bbea31d1d92e6e757765f
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-04-02 16:31:37 +0200

    allow hosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["mistral-ai-game-jam-description-improv.hf.space", "think-in-sync.com"],
   },
   plugins: [react()],
   resolve: {
